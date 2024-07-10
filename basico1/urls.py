4cf9b1d27ad71cfa6a47941f62970f6098c4839e
hiure <hiure@riseup.net>
2024-07-10 07:27:54 -0300

    add: url para o app dentro do projeto

--- basico1/urls.py
+++ basico1/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('par_impar/', include('par_impar.urls')),
+    path('funcoes/', include('funcoes.urls')),
 ]
