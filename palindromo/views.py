6ffe2f021e0acea7dfa9758ffc2ae6f694f1e473
hiure <hiure@riseup.net>
2024-07-12 06:34:10 -0300

    adiciona uma view para o aplicativo django

--- palindromo/views.py
+++ palindromo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Função que identifica se a palavra é um palindromo
+def identifica_palindromo(palavra):
+    # palavra[::-1] é uma técnica de slicing que inverte a string. A função verifica se a palavra é igual à sua versão invertida.
+    return palavra == palavra[::-1]
+
+# view para utilizar a função que identifica se é palindromo
+def verificar_palindromo_view(request):
+    # Inicia a variave 'resultado' como None
+    resultado = None
+    #Verifica se o método da requisição é POST  (quando o formulário é enviado)
+    if request.method == 'POST':
+        palavra = request.POST.get('palavra')
+        resultado = identifica_palindromo(palavra)
+    return render(request, 'palindromo/verificar_palindromo.html', {'resultado': resultado})
