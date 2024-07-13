6395b57a5f47dee30bb21ddb8f638e68fccddcbb
hiure <hiure@riseup.net>
2024-07-13 20:01:55 -0300

    adiciona funcionalidade de contar vogais ao app manipula_string

--- manipula_string/urls.py
+++ manipula_string/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inverter_palavra/', views.inverter_palavra_view, name='inverter_palavra')
+    path('inverter_palavra/', views.inverter_palavra_view, name='inverter_palavra'),
+    path('contar_vogais/', views.contar_vogais_view, name='contar_vogais'),
 ]
--- manipula_string/views.py
+++ manipula_string/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Função que inverte as letras da palava
 def inverter_palavra(palavra):
     return palavra[::-1]
+
+# Função que conta as vogais da palavra
+def contar_vogais(string):
+    # define vogais
+    vogais = "aeiouAEIOU"
+    #Contador de vogais
+    contagem = 0
+    # loop para percorrer a palavra e verificar quantas vogais
+    for char in string:
+        if char in vogais:
+            contagem += 1
+    # retorna a quantidade de vogais
+    return contagem
 
 # View que utiliza a função inverter_palavra
 def inverter_palavra_view(request):
@@ -16,3 +29,13 @@ def inverter_palavra_view(request):
         resultado = inverter_palavra(palavra)
     # Renderiza o template 'inverter_palavra.html', passando 'resultado' para o contexto
     return render(request, 'manipula_string/inverter_palavra.html', {'resultado': resultado})
+
+
+# View que utiliza a função contar_vogais
+def contar_vogais_view(request):
+    # zera o resultado
+    resultado = None
+    if request.method == 'POST':
+        string = request.POST.get('string')
+        resultado = contar_vogais(string)
+    return render(request, 'manipula_string/contar_vogais.html', {'resultado': resultado})
