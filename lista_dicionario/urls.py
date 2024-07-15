7165e07c428d8c337171250ab4942b8d83aec2b1
hiure <hiure@riseup.net>
2024-07-14 21:39:42 -0300

    adiciona uma visualização para contar palavras em uma frase

--- lista_dicionario/urls.py
+++ lista_dicionario/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('ordenar_numeros/', views.ordenar_numeros_view, name='ordenar_numeros'),
+    path('contar_palavras/', views.contar_palavras_view, name='contar_palavras'),
 ]
--- lista_dicionario/views.py
+++ lista_dicionario/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from collections import Counter
 
 # função que ordena numeros
 def ordenar_numeros(numeros):
@@ -6,9 +7,23 @@ def ordenar_numeros(numeros):
     numeros.sort()
     return numeros
 
+def contar_palavras(frase):
+    palavras = frase.split()
+
+    frequencia = Counter(palavras)
+    return frequencia
+
+# view que recebe as informações do formulário e utilzia a função que ordena os numeros e devolve 
 def ordenar_numeros_view(request):
     resultado = None
     if request.method == 'POST':
         numeros = request.POST.get('numeros').split(',')
         resultado = ordenar_numeros(numeros)
     return render(request, 'lista_dicionario/ordenar_numeros.html', {'resultado': resultado})
+
+def contar_palavras_view(request):
+    resultado = None
+    if request.method == 'POST':
+        frase = request.POST.get('frase')
+        resultado = contar_palavras(frase)
+    return render(request, 'lista_dicionario/contar_palavras.html', {'resultado': resultado})
